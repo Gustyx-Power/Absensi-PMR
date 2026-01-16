a890f05475cb6f041fb83cd340b54420db14041c
Gusti Aditya Muzaky <142430633+Gustyx-Power@users.noreply.github.com>
2026-01-17 04:57:02 +0700

    feat(config): add timezone setting Asia/Jakarta

--- config/database.php
+++ config/database.php
@@ -4,39 +4,47 @@
  * PMR Attendance System
  */
 
-class Database {
+// Set timezone to WIB (Western Indonesian Time)
+date_default_timezone_set('Asia/Jakarta');
+
+class Database
+{
     private $host = 'localhost';
     private $username = 'root';
     private $password = '';
     private $database = 'absensi_pmr';
     private $charset = 'utf8mb4';
-    
+
     public $conn;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->connect();
     }
-    
-    private function connect() {
+
+    private function connect()
+    {
         $this->conn = new mysqli(
             $this->host,
             $this->username,
             $this->password,
             $this->database
         );
-        
+
         if ($this->conn->connect_error) {
             die("Koneksi database gagal: " . $this->conn->connect_error);
         }
-        
+
         $this->conn->set_charset($this->charset);
     }
-    
-    public function getConnection() {
+
+    public function getConnection()
+    {
         return $this->conn;
     }
-    
-    public function close() {
+
+    public function close()
+    {
         if ($this->conn) {
             $this->conn->close();
         }
